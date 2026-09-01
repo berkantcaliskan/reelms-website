8ac79f00518bab80ba1823cff5b59948adaae4fe
Berkant <berkantcaliskn@gmail.com>
2026-09-01 11:12:41 +0300

    style(background): reduce grain opacity to 0.38 for softer cinematic texture

--- src/components/AmbientGradientBackground.tsx
+++ src/components/AmbientGradientBackground.tsx
@@ -139,7 +139,7 @@ export default function AmbientGradientBackground() {
             pointerEvents: "none",
             backgroundImage: `url(${grainDataUrl})`,
             backgroundRepeat: "repeat",
-            opacity: 0.60,
+            opacity: 0.38,
             mixBlendMode: "screen",
           }}
         />
